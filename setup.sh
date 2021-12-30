382fa824982178259db3f047940e2607856521c7
ZeroHott <75989815+ZeroHott@users.noreply.github.com>
2021-12-30 02:45:00 +0100

    imporoved

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ apt-get install \
     gnupg \
     lsb-release -y
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
