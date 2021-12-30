739c05bb80a68810a403a773af162e11a42dfe64
ZeroHott <75989815+ZeroHott@users.noreply.github.com>
2021-12-30 03:29:48 +0100

    removed fish commands

--- setup.sh
+++ setup.sh
@@ -34,10 +34,3 @@ git clone https://gist.github.com/8b1635d6c6d711ff120dbf5808bc2b71.git /home/$SU
 su $SUDO_USER
 
 echo 'alias up "bash ~/.upgrade/upgrade.sh"' >> /home/$SUDO_USER/.config/fish/config.fish
-
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-fisher install simnalamburt/shellder
-fisher install jethrokuan/z
-wget https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip
-sudo unzip -q exa-linux-x86_64-v0.10.1.zip bin/exa -d /usr/local
-apt-get install peco
