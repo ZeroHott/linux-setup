0d773e9cab82949a21b7fe569fbc73f61a2c3860
ZeroHott <75989815+ZeroHott@users.noreply.github.com>
2021-12-30 15:08:10 +0100

    added fisher pluging

--- fish-setup.sh
+++ fish-setup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/fish
 
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-fisher install simnalamburt/shellder
-fisher install jethrokuan/z
 wget https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip
 sudo unzip -q exa-linux-x86_64-v0.10.1.zip bin/exa -d /usr/local
-apt-get install peco
+fisher install jethrokuan/z
+fisher install joseluisq/gitnow
+git clone https://github.com/ryanoasis/nerd-fonts
+./nerd-fonts/install.sh SourceCodePro
