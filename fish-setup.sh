c66b141754653e9ad09488ad9e73672a6fa585ff
ZeroHott <75989815+ZeroHott@users.noreply.github.com>
2022-01-01 23:22:47 +0100

    Delete fish-setup.sh

--- fish-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/fish
-
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-wget https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip
-sudo unzip -q exa-linux-x86_64-v0.10.1.zip bin/exa -d /usr/local
-fisher install jethrokuan/z
-fisher install joseluisq/gitnow
-fisher install oh-my-fish/theme-l
-exit
